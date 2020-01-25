41c6dbb936d95c2cc202badae63072f5b53d8aab
Juan G <hello@juangonzalez.com.au>
2020-01-26 09:13:01 +1100

    adding search/replace plugin

---
+++ plugins/local-plugin/gatsby-node.js
@@ -0,0 +1,73 @@
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+});
+
+const _ = require("lodash");
+const replace = require('replace-in-file');
+
+exports.onPostBuild = () =>{
+
+    const folders = [
+        'public/*.html',
+        'public/*.json',
+        'public/*/*.html',
+        'public/*/*.json',
+        'public/*/*/*.html',
+        'public/*/*/*.json',
+        'public/*/*/*/*.html',
+        'public/*/*/*/*.json',
+    ];
+
+    const regex = new RegExp(`${  process.env.GATSBY_GHOST_API_URL  }`, "g");
+    const options = {
+        files: folders,
+        from: regex,
+        to: process.env.GATSBY_FINAL_URL,
+        countMatches: true,
+        ignore: [
+            'public/*.gz','public/*.map'
+        ]
+    };
+    try {
+        const results = replace.sync(options);
+
+        _.mapValues(results, (o) => {
+            if (o.hasChanged) {
+                console.log(`Matched ${  o.numMatches} - Replaced ${  o.numReplacements} | Replaced "${  process.env.GATSBY_GHOST_API_URL   }" with "${  process.env.GATSBY_FINAL_URL}" in ${  o.file}.`)
+            }
+        });
+    }
+    catch (error) {
+        console.error('Error occurred:', error);
+    }
+
+    if (process.env.GATSBY_REPLACE_STRING_WHAT && process.env.GATSBY_REPLACE_STRING_WITH) {
+
+
+        const string = new RegExp(`${  process.env.GATSBY_REPLACE_STRING_WHAT }`, "g");
+        const settings = {
+            files: folders,
+            from: string,
+            to: process.env.GATSBY_REPLACE_STRING_WITH,
+            countMatches: true,
+            ignore: [
+                'public/*.gz','public/*.map'
+            ]
+        };
+        try {
+            const results = replace.sync(settings);
+         _.mapValues(results, (o) => {
+             if (o.hasChanged) {
+                 console.log(`Matched ${  o.numMatches} - Replaced ${  o.numReplacements} | Replaced "${  process.env.GATSBY_REPLACE_STRING_WHAT  }" with "${  process.env.GATSBY_REPLACE_STRING_WITH}" in ${  o.file}.`)
+             }
+         });
+            // => { 'fred': 40, 'pebbles': 1 } (iteration order is not guaranteed)
+
+        }
+        catch (error) {
+            console.error('Error occurred:', error);
+        }
+
+    }
+
+};
