49f8765d5c80a4fef0fd920e2c7645a7c115d972
Juan G <hello@juangonzalez.com.au>
2020-01-28 14:08:37 +1100

    small changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,8 @@ module.exports = {
           },
           "gatsby-remark-prismjs",
           "gatsby-remark-copy-linked-files",
-          "gatsby-remark-autolink-headers"
+          "gatsby-remark-autolink-headers",
+          "gatsby-plugin-catch-links"
         ]
       }
     },
